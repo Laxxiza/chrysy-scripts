894b6491c028a6d9ca414a5898cf2439bcca46bd
Laxxiza <62009001+Laxxiza@users.noreply.github.com>
2021-09-01 11:42:38 +0300

    Create maphelper.user.js

---
+++ maphelper.user.js
@@ -0,0 +1,27 @@
+function etrRgx({findPath, findValue}, text) {
+    const str = text;
+    if(str.match(findPath) && str.match(findValue)){
+        let result = str.match(findPath)[0];
+        result = result.match(findValue);
+        return result.join(',');
+    }
+    return "";
+}
+
+function map(event) {
+    const findPath = /\([^\(]+(poi|home)[^\)]*\)/mi;
+    const findValue = /\d+\.\d+/gmi;
+    const yaMapUrl = `https://maps.yandex.ru/?mode=search&text=`;
+    const text = event.target.innerText;
+    const cordMap = etrRgx({findPath, findValue}, text);
+
+    if (cordMap) {
+        console.log('%c%s', 'font: bold 2em/1 Arial; color: green', 'Координаты найдены');
+        window.open(yaMapUrl + cordMap);
+    }
+    else console.log('%c%s', 'font: bold 2em/1 Arial; color: red', 'Координаты не найдены');
+}
+
+$(document).ready(function(){
+    $(document).on('click', 'td.trim', map);
+});
